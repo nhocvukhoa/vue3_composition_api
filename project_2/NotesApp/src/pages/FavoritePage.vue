618f9b797523dcea9d39210973090983f756bb2c
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-06-19 15:08:23 +0700

    remove favorite note with api

--- project_2/NotesApp/src/pages/FavoritePage.vue
+++ project_2/NotesApp/src/pages/FavoritePage.vue
@@ -1,29 +1,28 @@
 <template>
-  <div class="bin-page">
-    <h3>Total: {{ totalBin }}</h3>
+  <div class="favorite-page">
+    <h3>Total: {{ totalfavorite }}</h3>
     <div class="row">
-      <div v-for="item in bin.data" :key="item.id" class="col-lg-4 col-xl-3 col-sm-6">
-        <div class="bin-content" :style="{ backgroundColor: item.background_color }">
-          <p>{{ item.text }}</p>
+      <div v-for="item in favorite.data" :key="item.id" class="col-lg-4 col-xl-3 col-sm-6">
+        <div class="favorite-content" :style="{ backgroundColor: item.note.background_color }">
+          <p>{{ item.note.text }}</p>
           <div class="action">
-            <button class="edit" @click="restore(item.id)">Restore</button>
             <button class="delete" @click="destroy(item.id)">Delete</button>
           </div>
         </div>
       </div>
     </div>
 
-    <Bootstrap4Pagination style="justify-content: center;" :data="bin"
-      @pagination-change-page="getBin" />
+    <Bootstrap4Pagination style="justify-content: center;" :data="favorite"
+      @pagination-change-page="getfavorite" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.bin-page {
+.favorite-page {
   width: 100%;
   margin: 10px 0 10px 0;
 
-  .bin-content {
+  .favorite-content {
     width: 100%;
     height: 250px;
     position: relative;
@@ -46,16 +45,10 @@
       padding-top: 10px;
       border-top: 1px solid black;
 
-      .edit,
       .delete {
         border: none;
         padding: 10px 20px;
         cursor: pointer;
-      }
-
-      .edit {
-        background-color: #0068d7;
-        margin-right: 10px;
       }
 
       .delete {
@@ -74,50 +67,33 @@ import { Bootstrap4Pagination } from "laravel-vue-pagination";
 
 const router = useRouter();
 
-const bin = ref([]);
-const totalBin = ref();
+const favorite = ref([]);
+const totalfavorite = ref();
 
-const getBin = async (page = 1) => {
+const getfavorite = async (page = 1) => {
   await axios
     .get(
-      `http://127.0.0.1:8000/api/bin?page=${page}`
+      `http://127.0.0.1:8000/api/favorite?page=${page}`
     )
     .then((response) => {
-      bin.value = response.data.data;
+      favorite.value = response.data.data;
       console.log(response.data.data)
-      totalBin.value = response.data.data.total;
+      totalfavorite.value = response.data.data.total;
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-getBin();
-
-const restore = async (id) => {
-  if (confirm("Are you sure you want to restore note ?")) {
-    await axios
-      .put(`http://127.0.0.1:8000/api/bin/restore/${id}`)
-      .then((response) => {
-        if (response.status == 200) {
-          getBin();
-          console.log("Restore note success");
-        }
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  }
-}
+getfavorite();
 
 const destroy = async (id) => {
-  if (confirm("Are you sure you want to destroy note forever ?")) {
+  if (confirm("Are you sure you want to destroy note favorite ?")) {
     await axios
-      .delete(`http://127.0.0.1:8000/api/bin/${id}`)
+      .delete(`http://127.0.0.1:8000/api/favorite/${id}`)
       .then((response) => {
         if (response.status == 200) {
-          getBin();
-          console.log("Destroy note forever success");
+          getfavorite();
         }
       })
       .catch((error) => {
